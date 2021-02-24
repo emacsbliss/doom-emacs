82963fcee21c5dbc51b932e6f2a17b6ce917800e
emacsbliss <emacsbliss@gmail.com>
2021-02-24 18:35:02 -0500

    bump emacseverywhere version

--- modules/app/everywhere/packages.el
+++ modules/app/everywhere/packages.el
@@ -3,4 +3,4 @@
 
 (package! emacs-everywhere
   :recipe (:host github :repo "tecosaur/emacs-everywhere")
-  :pin "d84b397c9a82c2fea1b0c87740c395a2c7af3be8")
+  :pin "87eb6278e59a2882c2fea1b0c87740c395a2c7af3be8")
